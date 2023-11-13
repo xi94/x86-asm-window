2d4d7a2d832bd02ef1bfad635a5615af095b94cf
christian <49824711+xi94@users.noreply.github.com>
2023-11-13 01:27:52 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <Windows.h>
+#include <memory>
 
+#include "x86_asm_window.hpp"
+
+#if 1
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+   std::unique_ptr<char[]> name(new char[] {"hello, world!"});
+   window_asm_x86 w(name.get(), 720, 580);
+
+   return 0;
+}
+
+#else
+
+#include "c/window.h"
+
+int main(int argc, char** argv)
+{
+   Window *w = NULL;
+   int r = ht2_initialize_window(L"my title", 0, w);
+   return 0;
+}
+
+#endif
